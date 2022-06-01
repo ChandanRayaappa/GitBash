d11f2d887a8436749c19e646d201c92bb1b054d2
Chandankumar <chandanraayappab@gmail.com>
2022-06-01 12:28:38 +0530

    first commit

---
+++ src/test/java/GitBash/GitBashTest.java
@@ -0,0 +1,12 @@
+package GitBash;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitBashTest {
+	@Test
+	public void gitBash() {
+		Reporter.log("Hello push the data into github using comd line",true);
+	}
+
+}
